8dc0c866df83b2f220cf786d0a623f67c41c0928
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2021-01-18 19:04:37 +0900

    通常でプレイブラストが起動するように設定。

--- mayaRender.py
+++ mayaRender.py
@@ -45,9 +45,9 @@ def secenece(cameraShape = [], fileName = '', width = 1920, height = 1080, image
     cmds.setAttr('defaultRenderGlobals.periodInExt', 2)
     cmds.render(b = True, rep = True)
 
-def runPlayblast(cam = '', fileName = '', width = 1920, height = 1080, workSpacePath = '', startFrame = 0, endFrame = 30):
+def runPlayblast(cam = '', fileName = '', width = 1920, height = 1080, workSpacePath = '', startFrame = 0, endFrame = 3):
     mel.eval('setProject "%s";' % workSpacePath)
-    #cmds.lookThru(cam)
+    cmds.lookThru(cam)
     path = cmds.workspace(q=True,rootDirectory=1)+'movies/'
     cmds.playblast(st = startFrame, et = endFrame, fo = True, w = width, h = height, v = False, c = 'h264', orn = True, fmt = 'qt', p = 100, f = path + fileName)
 

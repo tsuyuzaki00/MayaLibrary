80e4d9c76de46182a421093fe3fd29f713958534
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2021-02-16 19:08:46 +0900

    jsonフォルダをsetProject内に作成するスクリプトを追加

--- mayaJsons.py
+++ mayaJsons.py
@@ -1,11 +1,34 @@
 import pymel.core as pm
+import maya.cmds as cmds
 import json
+import os
 from collections import OrderedDict as od
 
 class MayaJson:
     def __init__(self):
         #filePass = 'C:/Users/tsuyuzaki.tatsuya/Desktop/test/output.json'
         self.filePass = 'testScripts/jsonTest.json'
+
+    def createFileJson(self):
+        dataPath = cmds.workspace(q=True,rootDirectory=1)+'data/'
+        files = os.listdir(dataPath)
+        if 'json' in files:
+            jsonPath = dataPath + 'json/'
+        else:
+            os.mkdir(dataPath + 'json')
+            jsonPath = dataPath + 'json/'
+
+        files = os.listdir(jsonPath)
+        filepath = cmds.file(q=True, sn=True)
+        filename = os.path.basename(filepath)
+        raw_name, extension = os.path.splitext(filename)
+        if raw_name in files:
+            getJsons = jsonPath + raw_name + '/'
+        else:
+            os.mkdir(jsonPath + raw_name)
+            getJsons = jsonPath + raw_name + '/'
+        
+        return getJsons
 
     def writeJson(self):
         s =  ["help_grp_mb", "guide"],{
@@ -24,3 +47,38 @@ class MayaJson:
         s = {"parent":parent, "child":child}
         with open(self.filePass, 'w') as f:
             json.dump(s, f, indent = 4, ensure_ascii = False)
+
+    def jointHierarchyExportJson(self):
+        jsonPath = self.createFileJson()
+        sels = pm.selected()
+        s = []
+        setNode = {}
+        margeNode = {}
+        timeNode = {"key":1}
+
+        for sel in sels:
+            selLists = pm.listAttr(k=True)
+            for selList in selLists:
+                selAttr = pm.getAttr(sel + '.' + selList)
+                keyNode = {selList:round(selAttr,3)}
+                setNode.update(keyNode)
+                objDict = {str(sel):setNode}
+            margeNode.update(objDict)
+            timeNode.update(margeNode)
+        s.append(timeNode)
+
+        with open(jsonPath + 'jointHierarchy.json', 'w') as f:
+        	json.dump(s, f, indent = 4, ensure_ascii = False)
+
+    def jointHierarchyImportJson(self):
+        jsonPath = self.createFileJson()
+        with open(jsonPath + 'jointHierarchy.json', 'r') as f:
+            roots = json.load(f)
+            for root in roots:
+                keys = root.keys()
+                for key in keys:
+                    pm.setAttr(key + ".rotateX", root[key]["rotateY"])
+                    pm.setAttr(key + ".rotateY", root[key]["rotateY"])
+                    pm.setAttr(key + ".rotateZ", root[key]["rotateZ"])
+                #pm.setAttr('leg_L0_fk0_ctl.rotateZ', root["Character1_LeftLeg"]["rotateZ"])
+
